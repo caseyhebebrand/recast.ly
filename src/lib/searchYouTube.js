4e11e78199fcafc6d1c06127abe1bd258a668a10
Hack Reactor Student <student@hackreactor.com>
2017-09-08 17:01:32 -0700

    connect to YouTube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  return $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search/',
+    type: 'GET',
+    data: {
+      'key': options.key,
+      'maxResults': options.max,
+      'q': options.query,
+      'part': 'snippet',
+      'type': 'video',
+      'videoEmbeddable': true
+    },
+    contentType: 'application/json',
+    success: (data) => {
+      console.log('success', data);
+      callback(data.items);
+    },
+    error: (e) => {
+      console.log('error', e);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
